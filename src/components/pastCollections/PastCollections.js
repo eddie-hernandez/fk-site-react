554fbd004947b0f3a175ee86e5a841989be1ff3c
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2023-11-03 17:49:45 -0400

    adding slider on archive carousel

--- src/components/pastCollections/PastCollections.js
+++ src/components/pastCollections/PastCollections.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import collections from '../../collections.json'
 import Slider from 'react-slick'
+import Draggable from 'react-draggable'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import './PastCollections.css'
 
 export default function PastCollections() {
+  const sliderRef = useRef()
+  const [currentSlide, setCurrentSlide] = useState(0)
+
   const settings = {
     infinite: true,
     centerMode: true,
     centerPadding: '0',
     slidesToShow: 3,
-    speed: 500,
+    speed: 200,
     initialSlide: 0,
+    arrows: false,
+    beforeChange: (oldIndex, newIndex) => {
+      setCurrentSlide(newIndex)
+    },
+  }
+
+  const getSlideStyles = (index) => {
+    const distance = index - currentSlide
+    const angle = distance * 30
+
+    return {
+      transform: `perspective(1000px) rotateY(${angle}deg)`,
+      transition: 'transform 0.3s ease',
+    }
+  }
+
+  const handleDrag = (e, ui) => {
+    const sliderNode = sliderRef.current
+    const movement = ui.deltaX
+    if (movement < 1) {
+      sliderNode.slickPrev()
+    } else if (movement > -1) {
+      sliderNode.slickNext()
+    }
   }
 
   return (
@@ -20,16 +48,31 @@ export default function PastCollections() {
       <h3 className="collectionTitle">
         <u>PAST COLLECTIONS</u>
       </h3>
-      <Slider {...settings}>
-        {collections.map((collection, index) => (
-          <div key={index} className="collection-slide">
-            <img src={collection.imageSrc} alt={collection.collectionTitle} />
-            <div className="collection-title">
-              <h6 className='pastCollectionTitle'>{collection.collectionTitle}</h6>
+      <div className="carouselWrapper">
+        <Slider ref={sliderRef} {...settings}>
+          {collections.map((collection, index) => (
+            <div
+              key={index}
+              className="collection-slide"
+              style={getSlideStyles(index)}
+            >
+              <img src={collection.imageSrc} alt={collection.collectionTitle} />
+              <div className="collection-title">
+                <h6 className="pastCollectionTitle">
+                  {collection.collectionTitle}
+                </h6>
+              </div>
             </div>
-          </div>
-        ))}
-      </Slider>
+          ))}
+        </Slider>
+      </div>
+      <div className="dragWrapper">
+        <div className="dragHandleContainer">
+          <Draggable axis="x" onDrag={handleDrag} bounds="parent">
+            <div className="dragHandle" />
+          </Draggable>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/pastEvents/PastEvents.js
+++ src/components/pastEvents/PastEvents.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import events from '../../events.json'
 import Slider from 'react-slick'
+import Draggable from 'react-draggable'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import './PastEvents.css'
 
 export default function PastEvents() {
+  const sliderRef = useRef()
+  const [currentSlide, setCurrentSlide] = useState(0)
+
   const settings = {
     infinite: true,
     centerMode: true,
@@ -13,6 +17,17 @@ export default function PastEvents() {
     slidesToShow: 3,
     speed: 500,
     initialSlide: 0,
+    arrows: false,
+  }
+
+  const handleDrag = (e, ui) => {
+    const sliderNode = sliderRef.current
+    const movement = ui.deltaX
+    if (movement < 1) {
+      sliderNode.slickPrev()
+    } else if (movement > -1) {
+      sliderNode.slickNext()
+    }
   }
 
   return (
@@ -20,16 +35,25 @@ export default function PastEvents() {
       <h3 className="collectionTitle">
         <u>PAST EVENTS</u>
       </h3>
-      <Slider {...settings}>
-        {events.map((event, index) => (
-          <div key={index} className="event-slide">
-            <img src={event.imageSrc} alt={event.eventTitle} />
-            <div className="event-title">
-              <h6 className="pastEventTitle">{event.eventTitle}</h6>
+      <div className="carouselWrapper">
+        <Slider ref={sliderRef} {...settings}>
+          {events.map((event, index) => (
+            <div key={index} className="event-slide">
+              <img src={event.imageSrc} alt={event.eventTitle} />
+              <div className="event-title">
+                <h6 className="pastEventTitle">{event.eventTitle}</h6>
+              </div>
             </div>
-          </div>
-        ))}
-      </Slider>
+          ))}
+        </Slider>
+      </div>
+      <div className="dragWrapper">
+        <div className="dragHandleContainer">
+          <Draggable axis="x" onDrag={handleDrag} bounds="parent">
+            <div className="dragHandle" />
+          </Draggable>
+        </div>
+      </div>
     </div>
   )
 }
